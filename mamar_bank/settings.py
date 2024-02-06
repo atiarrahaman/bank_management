48d86c53f3f6e239f99c643b91449812cff38ee0
atiarrahaman <atiar3393@gmail.com>
2024-02-06 22:00:31 +0300

    host add

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--=+7!o22oh2d!@ngrls(u)ikz$qaxhv^wo8&5&2+rgjhm8n2rk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
